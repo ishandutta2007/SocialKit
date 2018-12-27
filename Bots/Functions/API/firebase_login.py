a3e5242f854888501fa88b3640661e425b9890b9
Vlad <vlad.j.work@gmail.com>
2018-12-27 18:43:19 -0500

    Small  Changes

---
+++ Bots/Functions/API/firebase_login.py
@@ -0,0 +1,10 @@
+import firebase_admin
+
+from firebase_admin import credentials
+
+cred = credentials.Certificate('serviceAccountKey.json')
+firebase_admin.initialize_app(cred, {"databaseURL": "https://social-kit-23456.firebaseio.com"})
+
+firebase_admin.initialize_app(cred, {
+    'databaseURL' : 'https://social-kit-23456.firebaseio.com'
+})
